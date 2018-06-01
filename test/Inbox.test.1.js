6b69aabb9d2772db98de55612a45e5ec7fc1c65c
Wen Xing Pan <w.pan@alumni.ubc.ca>
2018-06-01 16:56:04 -0700

    Initial Commit

---
+++ test/Inbox.test.1.js
@@ -0,0 +1,45 @@
+// const assert = require('assert');
+// const ganache = require('ganache-cli')
+// const Web3 = require('web3')
+
+// const rpc = "ws://localhost:8545"
+// const web3 = new Web3(rpc);
+// const {interface, bytecode} = require('../compile');
+
+// let accounts;
+// let inbox;
+// const INITIAL_MSG = "Hello World"
+
+// beforeEach(async() => {
+//     accounts = await web3.eth.getAccounts();
+//     // web3.eth.getAccounts().then(fetchedAccounts => {
+//     //     console.log(fetchedAccounts);
+//     // })
+
+//     inbox = await new web3.eth.Contract(JSON.parse(interface))
+//         .deploy({data: bytecode, arguments:[INITIAL_MSG]})
+//         .send({from: accounts[0], gas: "1000000"})
+
+// })
+
+// describe("Inbox", () => {
+//     it("Success contract deployment: ", () => {
+//         assert.ok(inbox.options.address)
+//         //console.log("address: ",inbox.options.address);
+//         //console.log("methods: ",inbox.methods);
+//     });
+
+//     it("Get message initialMessage: ", async() => {
+//         const message = await inbox.methods.message().call()
+//         assert.equal(message, INITIAL_MSG);
+//     });
+
+//     it("Set message: ", async()=>{
+//         const NEW_MSG = "Goodbye, World"
+//         const txHash = await inbox.methods.setMessage(NEW_MSG).send({from: accounts[1], gas: "1000000"})
+//         const message = await inbox.methods.message().call()
+//         console.log("txHash: ", txHash);
+//         console.log("message: ", message)
+//         assert.equal(message, NEW_MSG);
+//     })
+// });
---
+++ test/Mochasample.test.js
@@ -0,0 +1,27 @@
+// const assert = require('assert');
+
+// class Car {
+//     park(){
+//         return 'stopped';
+//     }
+
+//     drive(){
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+
+// beforeEach(()=>{
+//     car = new Car();
+// });
+
+// describe('Car', ()=>{
+//     it("Test Park", ()=>{
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it("Test Park", ()=>{
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
